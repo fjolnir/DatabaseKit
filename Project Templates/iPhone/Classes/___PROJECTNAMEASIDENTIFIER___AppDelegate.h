9e2aef73528a545b82efaa3760db653232771d24
Fjölnir Ásgeirsson <fjolnir@gmail.com>
2008-08-29 23:35:52 +0000

    Unpacked iPhone template

---
+++ Project Templates/iPhone/Classes/___PROJECTNAMEASIDENTIFIER___AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  ___PROJECTNAMEASIDENTIFIER___AppDelegate.h
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ___PROJECTNAMEASIDENTIFIER___ViewController;
+
+@interface ___PROJECTNAMEASIDENTIFIER___AppDelegate : NSObject <UIApplicationDelegate> {
+	IBOutlet UIWindow *window;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
+
