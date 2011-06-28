6991b0060ef57e0a102ccf60cf18428289534137
Fjölnir Ásgeirsson <f@ninjakitten.us>
2011-06-29 00:25:14 +0900

    Intermediate, total replacement of the unit tests, big commit and a pain in the ass so I'm splitting it up

---
+++ Unit tests/Unit_TestsAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  Unit_TestsAppDelegate.m
+//  Unit Tests
+//
+//  Created by Fjölnir Ásgeirsson on 6/28/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Unit_TestsAppDelegate.h"
+
+@implementation Unit_TestsAppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+  // Insert code here to initialize your application
+}
+
+@end
---
+++ Unit tests/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Unit Tests
+//
+//  Created by Fjölnir Ásgeirsson on 6/28/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+  return NSApplicationMain(argc, (const char **)argv);
+}
