7c8972ed5aa138e2afb9d6feaa8e3323cb7bb3ac
Fjölnir Ásgeirsson <f@ninjakitten.us>
2011-07-24 16:59:59 +0900

    Revert "Revert "Fixed compiler warnings in the Unit tests""
    This reverts commit 02fe655a8f2575ecec4cdf14835e4aa9887ce3cd.

--- Mac/Unit Tests/GHUnit/GHUnitTestMain.m
+++ Mac/Unit Tests/GHUnit/GHUnitTestMain.m
@@ -33,7 +33,7 @@
 #import <GHUnit/GHTestApp.h>
 
 // Default exception handler
-void exceptionHandler(NSException *exception) { 
+static void exceptionHandler(NSException *exception) { 
   NSLog(@"%@\n%@", [exception reason], GHUStackTraceFromException(exception));
 }
 
