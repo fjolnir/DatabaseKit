02def405b32e704d506e2406564555f0e0afb0ea
Fjölnir Ásgeirsson <fjolnir@asgeirsson.is>
2014-10-26 02:49:14 +0900

    Added relationship column for to one relationships

--- DatabaseKit/DBModel+Relationships.m
+++ DatabaseKit/DBModel+Relationships.m
@@ -1,16 +1,20 @@
 #import "DBModel+Relationships.h"
 #import "NSString+DBAdditions.h"
+#import <objc/runtime.h>
 
 @implementation DBModel (Relationships)
 + (Class)relatedClassForKey:(NSString *)key
 {
     Class klass;
-    if([self typeForKey:key class:&klass] == '@') {
+    if([self typeForKey:key class:&klass] == _C_ID) {
         if([klass isSubclassOfClass:[NSSet class]])
-            return NSClassFromString([@"TE" stringByAppendingString:[[key singularizedString] stringByCapitalizingFirstLetter]]);
+            return NSClassFromString([[self classPrefix] stringByAppendingString:[[key singularizedString] stringByCapitalizingFirstLetter]]);
+        else if([klass isSubclassOfClass:[DBModel class]])
+            return NSClassFromString([[self classPrefix] stringByAppendingString:[key stringByCapitalizingFirstLetter]]);
     }
     return nil;
 }
+
 + (NSString *)foreignKeyName
 {
     return [[[self tableName] singularizedString] stringByAppendingString:@"Identifier"];
