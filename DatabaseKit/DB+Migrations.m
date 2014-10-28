ed07a6c4d323e908d492487afe07cc1f6de6bd8e
Fjölnir Ásgeirsson <fjolnir@asgeirsson.is>
2014-10-28 17:56:48 +0900

    Implemented support for adding columns in a migration

--- DatabaseKit/DB+Migrations.m
+++ DatabaseKit/DB+Migrations.m
@@ -8,96 +8,114 @@
 static NSString * const kCYMigrationTableName = @"DBKitSchemaInfo";
 @implementation DB (Migrations)
 
+- (NSDictionary *)tableCreationQueriesForClasses:(NSArray *)classes
+{
+    NSMutableDictionary *creates = [NSMutableDictionary dictionaryWithCapacity:[classes count]];
+    for(Class klass in classes) {
+        NSMutableArray *columns = [NSMutableArray arrayWithObject:
+                                   [DBColumn columnWithName:@"identifier"
+                                                       type:@"TEXT"
+                                                constraints:@[[DBPrimaryKeyConstraint
+                                                               primaryKeyConstraintWithOrder:DBOrderAscending
+                                                               autoIncrement:NO
+                                                               onConflict:DBConflictActionFail]]]];
+        for(NSString *key in [klass savedKeys]) {
+            NSString *type = nil;
+            Class keyClass = nil;
+            char enc = [klass typeForKey:key class:&keyClass];
+            if(enc == _C_ID)
+                type = [[self.connection class] typeForClass:keyClass];
+            else
+                type = [[self.connection class] typeForObjCScalarEncoding:enc];
+            if(!type) {
+                if([keyClass isSubclassOfClass:[NSSet class]]) { // To many
+                    Class counterpart = [klass relatedClassForKey:key];
+                    if(counterpart && ![creates[[counterpart tableName]] hasColumnNamed:[klass foreignKeyName]]) {
+                        DBColumn *foreignKeyCol = [DBColumn
+                                                   columnWithName:[klass foreignKeyName]
+                                                   type:@"TEXT"
+                                                   constraints:@[[DBForeignKeyConstraint
+                                                                  foreignKeyConstraintWithTable:[klass tableName]
+                                                                  columnName:@"identifier"
+                                                                  onDelete:DBForeignKeyActionCascade
+                                                                  onUpdate:DBForeignKeyActionCascade]]];
+                        if(!creates[[counterpart tableName]])
+                            creates[[counterpart tableName]] = [[[self create] table:[counterpart tableName]] columns:@[foreignKeyCol]];
+                        else {
+                            DBCreateQuery *q = creates[[klass tableName]];
+                            creates[[counterpart tableName]] = [q columns:[q.columns arrayByAddingObject:foreignKeyCol]];
+                        }
+                    }
+                } else if([keyClass isSubclassOfClass:[DBModel class]]) { // To one
+                    Class counterpart = [klass relatedClassForKey:key];
+                    if(counterpart && ![creates[[klass tableName]] hasColumnNamed:[counterpart foreignKeyName]]) {
+                        DBColumn *foreignKeyCol = [DBColumn
+                                                   columnWithName:[counterpart foreignKeyName]
+                                                   type:@"TEXT"
+                                                   constraints:@[[DBForeignKeyConstraint
+                                                                  foreignKeyConstraintWithTable:[counterpart tableName]
+                                                                  columnName:@"identifier"
+                                                                  onDelete:DBForeignKeyActionCascade
+                                                                  onUpdate:DBForeignKeyActionCascade]]];
+                        if(!creates[[klass tableName]])
+                            creates[[counterpart tableName]] = [[[self create] table:[counterpart tableName]] columns:@[foreignKeyCol]];
+                        else {
+                            DBCreateQuery *q = creates[[klass tableName]];
+                            creates[[klass tableName]] = [q columns:[q.columns arrayByAddingObject:foreignKeyCol]];
+                        }
+                    }
+                }
+                continue;
+            }
+
+            NSAssert(NSNotFound == [columns indexOfObjectPassingTest:^(DBColumn *col, NSUInteger _, BOOL *__) {
+                return [col.name isEqualToString:key];
+            }], @"Duplicate column %@", key);
+            [columns addObject:[DBColumn columnWithName:key
+                                                   type:type
+                                            constraints:[klass constraintsForKey:key]]];
+        }
+        if(!creates[[klass tableName]])
+            creates[[klass tableName]] = [[[self create] table:[klass tableName]] columns:columns];
+        else {
+            DBCreateQuery *q = creates[[klass tableName]];
+            creates[[klass tableName]] = [q columns:[q.columns arrayByAddingObjectsFromArray:columns]];
+        }
+    }
+    return creates;
+}
+
 - (BOOL)migrateModelClasses:(NSArray *)classes error:(NSError **)outErr
 {
     return [self.connection transaction:^{
-        NSMutableDictionary *creates = [NSMutableDictionary dictionaryWithCapacity:[classes count]];
+        NSMutableDictionary *creates = [[self tableCreationQueriesForClasses:classes] mutableCopy];
         for(Class klass in classes) {
             NSDictionary *lastMigration = [self currentMigrationForModelClass:klass error:outErr];
-            if(lastMigration) {
-                // TODO
-                return DBTransactionRollBack;
-            } else {
-                // Begin by adding all the simple columns (foreign keys handled later)
-                NSMutableArray *columns = [NSMutableArray arrayWithObject:
-                                           [DBColumn columnWithName:@"identifier"
-                                                               type:@"TEXT"
-                                                        constraints:@[[DBPrimaryKeyConstraint
-                                                                       primaryKeyConstraintWithOrder:DBOrderAscending
-                                                                       autoIncrement:NO
-                                                                       onConflict:DBConflictActionFail]]]];
-                for(NSString *key in [klass savedKeys]) {
-                    NSString *type = nil;
-                    Class keyClass = nil;
-                    char enc = [klass typeForKey:key class:&keyClass];
-                    if(enc == _C_ID)
-                        type = [[self.connection class] typeForClass:keyClass];
-                    else
-                        type = [[self.connection class] typeForObjCScalarEncoding:enc];
-                    if(!type) {
-                        if([keyClass isSubclassOfClass:[NSSet class]]) { // To many
-                            Class counterpart = [klass relatedClassForKey:key];
-                            if(counterpart && ![creates[[counterpart tableName]] hasColumnNamed:[klass foreignKeyName]]) {
-                                DBColumn *foreignKeyCol = [DBColumn
-                                                           columnWithName:[klass foreignKeyName]
-                                                           type:@"TEXT"
-                                                           constraints:@[[DBForeignKeyConstraint
-                                                                          foreignKeyConstraintWithTable:[klass tableName]
-                                                                          columnName:@"identifier"
-                                                                          onDelete:DBForeignKeyActionCascade
-                                                                          onUpdate:DBForeignKeyActionCascade]]];
-                                if(!creates[[counterpart tableName]])
-                                    creates[[counterpart tableName]] = [[[self create] table:[counterpart tableName]] columns:@[foreignKeyCol]];
-                                else {
-                                    DBCreateQuery *q = creates[[klass tableName]];
-                                    creates[[counterpart tableName]] = [q columns:[q.columns arrayByAddingObject:foreignKeyCol]];
-                                }
-                            }
-                        } else if([keyClass isSubclassOfClass:[DBModel class]]) { // To one
-                            Class counterpart = [klass relatedClassForKey:key];
-                            if(counterpart && ![creates[[klass tableName]] hasColumnNamed:[counterpart foreignKeyName]]) {
-                                DBColumn *foreignKeyCol = [DBColumn
-                                                           columnWithName:[counterpart foreignKeyName]
-                                                           type:@"TEXT"
-                                                           constraints:@[[DBForeignKeyConstraint
-                                                                          foreignKeyConstraintWithTable:[counterpart tableName]
-                                                                          columnName:@"identifier"
-                                                                          onDelete:DBForeignKeyActionCascade
-                                                                          onUpdate:DBForeignKeyActionCascade]]];
-                                if(!creates[[klass tableName]])
-                                    creates[[counterpart tableName]] = [[[self create] table:[counterpart tableName]] columns:@[foreignKeyCol]];
-                                else {
-                                    DBCreateQuery *q = creates[[klass tableName]];
-                                    creates[[klass tableName]] = [q columns:[q.columns arrayByAddingObject:foreignKeyCol]];
-                                }
-                            }
-                        }
-                        continue;
-                    }
+            if(!lastMigration)
+                continue;
 
-                    NSAssert(NSNotFound == [columns indexOfObjectPassingTest:^(DBColumn *col, NSUInteger _, BOOL *__) {
-                        return [col.name isEqualToString:key];
-                    }], @"Duplicate column %@", key);
-                    [columns addObject:[DBColumn columnWithName:key
-                                                           type:type
-                                                    constraints:[klass constraintsForKey:key]]];
-                }
-                if(!creates[[klass tableName]])
-                    creates[[klass tableName]] = [[[self create] table:[klass tableName]] columns:columns];
-                else {
-                    DBCreateQuery *q = creates[[klass tableName]];
-                    creates[[klass tableName]] = [q columns:[q.columns arrayByAddingObjectsFromArray:columns]];
-                }
+            NSArray *currentColumns = [NSKeyedUnarchiver unarchiveObjectWithData:lastMigration[@"columns"]];
+            NSArray *columnsToAdd = [[creates[[klass tableName]] columns]
+                                     filteredArrayUsingPredicate:[NSPredicate predicateWithBlock:^BOOL(DBColumn *col, NSDictionary *bindings) {
+                return ![[currentColumns valueForKey:@"name"] containsObject:col.name];
+            }]];
+            if([columnsToAdd count] > 0) {
+                DBAlterQuery *alter = [[self[[klass tableName]] alter] appendColumns:columnsToAdd];
+                if(![alter execute:outErr])
+                    return DBTransactionRollBack;
             }
+            return DBTransactionRollBack;
         }
+
         for(NSString *tableName in creates) {
             if(![(DBCreateQuery *)creates[tableName] execute:outErr])
                 return DBTransactionRollBack;
+
             DBInsertQuery *migration = [[[self migrationTable]
              insert:@{
                  @"table": tableName,
-                 @"columns": [NSArchiver archivedDataWithRootObject:[creates valueForKey:@"columns"]]
-             }] or:DBInsertFallbackReplace];
+                 @"columns": [NSKeyedArchiver archivedDataWithRootObject:[creates[tableName] valueForKey:@"columns"]]
+             }] or:DBInsertFallbackAbort];
             if(![migration execute:outErr])
                 return DBTransactionRollBack;
         }
