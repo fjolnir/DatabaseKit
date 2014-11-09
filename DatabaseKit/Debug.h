268a137c67191c390b4a8424bb7361498a491e36
Fjölnir Ásgeirsson <fjolnir@asgeirsson.is>
2014-11-09 14:51:36 +0900

    Cleanup

--- DatabaseKit/Debug.h
+++ DatabaseKit/Debug.h
@@ -1,16 +1,12 @@
 #include <unistd.h>
 
-// Some debug stuff
-// Crashesss
-#define CrashHerePlease() { *(int *)0 = 123; }
 #define DBLog(...) printf("%s(DBKit)[%u] %s: %s\n", [[[NSProcessInfo processInfo] processName] UTF8String], \
     getpid(),\
     [[NSString stringWithFormat:@"%s:%u", [[[NSString stringWithUTF8String:__FILE__] lastPathComponent] UTF8String], __LINE__] UTF8String],\
     [[NSString stringWithFormat:__VA_ARGS__] UTF8String])
 
 #ifdef ENABLE_DB_DEBUG
-// We make it a warning because when unit testing it's nice to see the logs in the list view instead of having to scroll through all the compiler output
-#define DBDebugLog(...) printf("%s: warning: %s\n", [[NSString stringWithFormat:@"%s:%u", [[[NSString stringWithUTF8String:__FILE__] lastPathComponent] UTF8String], __LINE__] UTF8String], [[NSString stringWithFormat:__VA_ARGS__] UTF8String])
+#  define DBDebugLog(...) DBLog(__VA_ARGS__)
 #else
 # define DBDebugLog(...) 
 #endif
