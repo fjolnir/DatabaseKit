7dbfe8c3dad01cf52703e597630af60f44da15a5
Fjölnir Ásgeirsson <fjolnir@asgeirsson.is>
2014-12-15 15:44:36 +0900

    Cleanup

--- Source/Utilities/DBIntrospection.m
+++ Source/Utilities/DBIntrospection.m
@@ -26,7 +26,7 @@ NSArray *DBClassesInheritingFrom(Class superclass)
     for(NSInteger i = 0; i < numClasses; i++) {
         Class kls = allClasses[i];
         const char *name = class_getName(kls);
-        if(!name || name[0] < 'A' || name[0] > 'Z' || strstr(name, "NSKVO") != NULL)
+        if(!name || name[0] < 'A' || name[0] > 'Z' || strncmp(name, "NSKVONotifying_", 15) == 0)
             continue;
         do {
             kls = class_getSuperclass(kls);
@@ -34,5 +34,7 @@ NSArray *DBClassesInheritingFrom(Class superclass)
         if(kls)
             [classes addObject:allClasses[i]];
     }
-    return classes;
+    return [classes count] > 0
+         ? classes
+         : nil;
 }
