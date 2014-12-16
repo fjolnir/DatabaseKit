de998b71f74581377a0c04954c6ba4c1f55ea8e1
Fjölnir Ásgeirsson <fjolnir@asgeirsson.is>
2014-12-16 17:20:10 +0900

    Added pendingQueries to private header

--- Source/DBModel+Private.h
+++ Source/DBModel+Private.h
@@ -3,6 +3,7 @@
 @interface DBModel ()
 @property(readwrite, strong) DB *database;
 @property(readwrite, strong) NSString *savedIdentifier;
+@property(readonly) NSDictionary *pendingQueries;
 
 - (BOOL)_save:(NSError **)outErr;
 @end
