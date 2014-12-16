68b716f1ac98e574599e1046bfa88c02924c7d28
Fjölnir Ásgeirsson <fjolnir@asgeirsson.is>
2014-12-17 00:00:41 +0900

    Implemented macro for defining abstract methods

--- Source/Utilities/DBUtilities.h
+++ Source/Utilities/DBUtilities.h
@@ -3,3 +3,8 @@
     __typeof(b) __b = (b); \
     __a == __b || [__a isEqual:__b]; \
 })
+
+#define DBNotImplemented() \
+    [NSException raise:NSInternalInconsistencyException \
+                format:@"%@ not implemented for %@", \
+                       NSStringFromSelector(_cmd), [self class]];
